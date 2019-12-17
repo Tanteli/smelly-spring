37660fb29e167b40f2bd1329fb1e3166ca2bb1dc
tanteli <andriatanteli@gmail.com>
2019-12-17 11:55:55 +0100

    Issue on SmellyUserController

--- smelly-spring/src/main/java/fr/formation/smellyspring/controllers/SmellyUserController.java
+++ smelly-spring/src/main/java/fr/formation/smellyspring/controllers/SmellyUserController.java
@@ -26,52 +26,52 @@ public class SmellyUserController {
     private final SmellyUserService service;
 
     protected SmellyUserController(SmellyUserService service) {
-	this.service = service;
+        this.service = service;
     }
 
     @GetMapping("/string")
     public String string() {
-	LOGGER.debug("Reached \"GET /string\"");
-	LOGGER.debug("Returning String=Hello smelly user!");
-	return "Hello smelly user!";
+        LOGGER.debug("Reached \"GET /string\"");
+        LOGGER.debug("Returning String=Hello smelly user!");
+        return "Hello smelly user!";
     }
 
     @GetMapping("/dto")
     public SmellyUserOutDto dto() {
-	LOGGER.debug("Reached \"GET /dto\"");
-	SmellyUserOutDto result = new SmellyUserOutDto(null, "Hello smelly user!");
-	LOGGER.debug("Returning dto=" + result);
-	return result;
+        LOGGER.debug("Reached \"GET /dto\"");
+        SmellyUserOutDto result = new SmellyUserOutDto(null, "Hello smelly user!");
+        LOGGER.debug("Returning dto=" + result);
+        return result;
     }
 
     @GetMapping("/service")
     public SmellyUserOutDto service() {
-	LOGGER.debug("Reached \"GET /service\"");
-	SmellyUserOutDto result = service.getSmellyUserOutDto();
-	LOGGER.debug("Returned from service.getSmellyUserOutDto()");
-	LOGGER.debug("Returning dto=" + result);
-	return result;
+        LOGGER.debug("Reached \"GET /service\"");
+        SmellyUserOutDto result = service.getSmellyUserOutDto();
+        LOGGER.debug("Returned from service.getSmellyUserOutDto()");
+        LOGGER.debug("Returning dto=" + result);
+        return result;
     }
 
     @PostMapping("/post")
     public void create(@Valid @RequestBody SmellyUserInDto dto) {
-	LOGGER.debug("Reached \"POST /\"");
-	LOGGER.debug("Verifying something with obscur code...");
-	something(dto);
-	LOGGER.debug("Something verified!");
-	SmellyUserOutDto result = service.create(dto);
-	LOGGER.debug("Returned from service.create(SmellyUserInDto)");
-	LOGGER.debug("Created smelly user with id 1L? Result=" + (result.getId().equals(1L)));
-	LOGGER.debug("Returning... void :)");
+        LOGGER.debug("Reached \"POST /\"");
+        LOGGER.debug("Verifying something with obscur code...");
+        something(dto);
+        LOGGER.debug("Something verified!");
+        SmellyUserInDto result = service.create(dto);
+        LOGGER.debug("Returned from service.create(SmellyUserInDto)");
+        LOGGER.debug("Created smelly user with id 1L? Result=" + (result.getId().equals(1L)));
+        LOGGER.debug("Returning... void :)");
     }
 
     private static void something(SmellyUserInDto dto) {
-	try {
-	    if (!dto.getClass().getDeclaredField("username").isAnnotationPresent(NotBlank.class)) {
-		throw new IllegalArgumentException("Stranger Things");
-	    }
-	} catch (NoSuchFieldException | SecurityException e) {
-	    throw new IllegalArgumentException("Stranger Things", e);
-	}
+        try {
+            if (!dto.getClass().getDeclaredField("username").isAnnotationPresent(NotBlank.class)) {
+                throw new IllegalArgumentException("Stranger Things");
+            }
+        } catch (NoSuchFieldException | SecurityException e) {
+            throw new IllegalArgumentException("Stranger Things", e);
+        }
     }
 }
